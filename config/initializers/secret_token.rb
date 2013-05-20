f973bd6fe0554f8b3ca0f977a4417232e55e446f
jm-code <j_maj@rocketmail.com>
2013-05-20 06:48:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c071f1ec128e86675dfcbb62b2621071087666d1a0f707338f951dcd75f59ac920d67a1f204e1a10fd60e569b546e2970f0e66be6b8b2d1909fdb011dd1ebadf'
